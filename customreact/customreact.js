8a8b96062066f0de1413893bcb3df7553363ebbb
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-16 22:10:09 +0530

    customreact

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,1 +1,17 @@
+function customRender(reactElement,container){
+    const domelement = document.createElement()
+}
+
+
+const reactElement = {
+    type:'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
 const maincontainer = document.getElementById("root");
+
+customRender(reactElement,maincontainer)
