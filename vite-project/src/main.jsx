c3ff0b22c4def8915429b9a533ef8e7f571773de
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-17 18:21:00 +0530

    custom React

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// import {jsx as _jsx} from "react/jsx-runtime.js"
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+function myapp() {
+    return (
+        <div>
+            <h1>Custom App | Chai</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: "a",
+//     props: {
+//       href: "https://google.com",
+//       target: "_blank",
+//     },
+//     children: "click me to visit google",
+// };
+
+  const anotherElement = (
+    <a href="http://google.com" target='_blank'>Visit google </a>
+  )
+
+  const anotheruser = "SBJOP"
+
+  const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target: "_blank"},
+    'click me to visit google.com',
+    anotheruser
+  )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+   reactElement
+
 )
