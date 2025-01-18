dc49835efbbe38aa1727d4dbec395c95c8116574
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-18 23:46:59 +0530

    if condtion in hook useState

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,17 +4,25 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(15);
+  let [counter, setCounter] = useState(10);
 
   // let counter = 15;
   const addValue = () => {
     // counter = counter + 1;
-    setCounter(counter + 1);
+    if (counter >= 20) {
+      setCounter(" Your counter goes out of range");
+    } else {
+      setCounter(counter + 1);
+    }
     console.log(counter);
   };
 
   const removevalue = () => {
-    setCounter(counter - 1);
+    if (counter > 0 && counter <= 20) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(" Your counter goes out of bound");
+    }
     console.log(counter);
   };
   return (
