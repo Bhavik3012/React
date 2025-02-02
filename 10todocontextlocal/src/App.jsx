6c417e549c6a086c5fc6817e8944fe2bd6e613a1
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-02-02 16:39:10 +0530

    Use aceternity ui in todo

--- 10todocontextlocal/src/App.jsx
+++ 10todocontextlocal/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
 import { TodoForm, TodoItem } from "./components/index";
-
+import { BackgroundBeamsWithCollision } from "./components/BackgroundBeamsWithCollision";
 export default function App() {
   const [todos, setTodos] = useState([]);
   const addTodo = (todo) => {
@@ -38,34 +38,36 @@ export default function App() {
   }, [todos]);
 
   return (
-    <TodoProvider
-      value={{
-        todos,
-        addTodo,
-        updateTodo,
-        deleteTodo,
-        toggleComplete,
-      }}
-    >
-      <div className="bg-[#172841] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
-          </h1>
-          <div className="mb-4">
-            {/* Todo form goes here */}
-            <TodoForm />
+    <div className="h-screen w-screen overflow-hidden">
+      <BackgroundBeamsWithCollision>
+        <TodoProvider
+          value={{
+            todos,
+            addTodo,
+            updateTodo,
+            deleteTodo,
+            toggleComplete,
+          }}
+        >
+          <div className="w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
+            <h1 className="text-4xl font-bold text-center mb-8 mt-2">
+              Manage Your Todos
+            </h1>
+            <div className="mb-4">
+              {/* Todo form goes here */}
+              <TodoForm />
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+              {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div className="w-full" key={todo.id}>
+                  <TodoItem todo={todo} />
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
-            {todos.map((todo) => (
-              <div className="w-full" key={todo.id}>
-                <TodoItem todo={todo} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </TodoProvider>
+        </TodoProvider>
+      </BackgroundBeamsWithCollision>
+    </div>
   );
 }
