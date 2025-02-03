427716095150f3daaf95aae56875124b46d5e105
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-02-03 21:44:53 +0530

    redux toolkit

---
+++ 11reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+function App() {
+  return (
+    <>
+      <h1>Learn about Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
