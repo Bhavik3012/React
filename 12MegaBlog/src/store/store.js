08faa758914209663a024be9314147efbd475873
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-02-16 19:03:00 +0530

    Debugging

--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authSlice from "./authSlice";
 const store = configureStore({
   reducer: {
-
+    auth: authSlice,
+    // TODO : add more slice here for posts and add post: postSlice
   },
-  
 });
 
 export default store;
