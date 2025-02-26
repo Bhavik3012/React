7afe0b610048c1406ca781e155aa8374c344f7ae
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-02-26 14:33:39 +0530

    re render of react

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -4,21 +4,30 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(1);
+  console.log("app rendered", Math.random());
+  // const [value, setValue] = useState(1);
   // const [multipliedvalue, setmultipliedvalue] = useState(1);
 
-  let multipiedValue = value * 5;
+  // let multipiedValue = value * 5;
 
-  const multiplybyfive = () => {
-    //  setmultipliedvalue(value * 5);
-    setValue(value + 1);
+  // const multiplybyfive = () => {
+  //   //  setmultipliedvalue(value * 5);
+  //   setValue(value + 1);
+  // };
+
+  const [value, setValue] = useState({
+    value: 0,
+  });
+  const clickMe = () => {
+    setValue({
+      value: 0,
+    });
   };
-
   return (
     <>
-      <h1>Main Value: {value}</h1>
-      <button onClick={multiplybyfive}>Click to mutiply by 5</button>
-      <h2>Multiplied Value: {multipiedValue}</h2>
+      <h1>Main Value: {value.value}</h1>
+      <button onClick={clickMe}>Click to mutiply by 5</button>
+      {/* <h2>Multiplied Value: {multipiedValue}</h2> */}
     </>
   );
 }
